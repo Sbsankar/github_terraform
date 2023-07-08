ced94aa4e05d03d9a82b89be7ee2a1e9171c96bc
Sbsankar <112096864+Sbsankar@users.noreply.github.com>
2023-07-08 09:06:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,7 @@
 provider "aws" {
    region = "ap-south-1"
 }
-resource "aws_instance" "myec2"
-  ami = "ami-0d13e3e640877b0b9"
-  instance_type = "t2.micro"
-}
-terraform {
-  backend "s3" {
-    bucket = "obitobucket"
-    key    = "sb/terraform.tfstate
-    region = "ap-south--1"
-  }
-}
-provider "aws" {
-   region = "ap-south-1"
-}
-resource "aws_instance" "myec2"
+resource "aws_instance" "myec2" {
   ami = "ami-0d13e3e640877b0b9"
   instance_type = "t2.micro"
 }
@@ -23,7 +9,6 @@ terraform {
   backend "s3" {
     bucket = "obitobucket"
     key    = "sb/terraform.tfstate"
-    region = "ap-south--1"
+    region = "ap-south-1"
   }
 }
-
